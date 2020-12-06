134feb2dff2dcbcc9272301dd0c6eaa9668abf5f
leor-w <leor_cao@163.com>
2020-12-07 01:34:34 +0800

    mod:完成客户端

--- user-cli/go.mod
+++ user-cli/go.mod
@@ -3,6 +3,8 @@ module github.com/leor-w/laracom/user-cli
 go 1.14
 
 require (
+	github.com/leor-w/laracom v1.0.3 // indirect
+	github.com/leor-w/laracom/user-service v0.0.0-20201206170533-592f590658e3 // indirect
 	github.com/micro/cli/v2 v2.1.2
 	github.com/micro/go-micro/v2 v2.9.1
 )
--- user-cli/main.go
+++ user-cli/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+
+	pb "github.com/leor-w/laracom/user-service/proto/user"
 	"github.com/micro/cli/v2"
 	"github.com/micro/go-micro/v2"
-	pb "github.com/leor-w/laracom/user-service/proto/user"
 )
 
 func main() {
@@ -24,5 +28,38 @@ func main() {
 		),
 	)
 
-	client := pb.
+	client := pb.NewUserService("laracom.user.service", service.Client())
+	service.Init(
+		micro.Action(func(c *cli.Context) error {
+			name := c.String("name")
+			email := c.String("email")
+			password := c.String("password")
+
+			log.Println("参数:", name, email, password)
+
+			r, err := client.Create(context.TODO(), &pb.User{
+				Name:     name,
+				Email:    email,
+				Password: password,
+			})
+			if err != nil {
+				log.Fatalf("创建用户失败: %v", err)
+			}
+			log.Printf("创建用户成功: %s", r.User.Id)
+
+			getAll, err := client.GetAll(context.Background(), &pb.Request{})
+			if err != nil {
+				log.Fatalf("获取所有用户失败: %v", err)
+			}
+			for _, v := range getAll.Users {
+				log.Println(v)
+			}
+			os.Exit(0)
+			return nil
+		}),
+	)
+
+	if err := service.Run(); err != nil {
+		log.Fatalf("用户端启动失败: %v", err)
+	}
 }
