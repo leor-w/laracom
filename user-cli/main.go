32f5293969ee6b861afed850cb40993b2faa1c69
leor-w <leor_cao@163.com>
2020-12-07 01:18:53 +0800

    mod:user-client

--- user-cli/main.go
+++ user-cli/main.go
@@ -1,22 +1,28 @@
 package main
 
+import (
+	"github.com/micro/cli/v2"
+	"github.com/micro/go-micro/v2"
+	pb "github.com/leor-w/laracom/user-service/proto/user"
+)
+
 func main() {
-	//service := micro.NewService(
-	//	micro.Flags(
-	//		&cli.StringFlag{
-	//			Name:  "name",
-	//			Usage: "Your name",
-	//		},
-	//		&cli.StringFlag{
-	//			Name:  "email",
-	//			Usage: "Your email",
-	//		},
-	//		&cli.StringFlag{
-	//			Name:  "password",
-	//			Usage: "Your password",
-	//		},
-	//	),
-	//)
-	//
-	//client := pb.
+	service := micro.NewService(
+		micro.Flags(
+			&cli.StringFlag{
+				Name:  "name",
+				Usage: "Your name",
+			},
+			&cli.StringFlag{
+				Name:  "email",
+				Usage: "Your email",
+			},
+			&cli.StringFlag{
+				Name:  "password",
+				Usage: "Your password",
+			},
+		),
+	)
+
+	client := pb.
 }
